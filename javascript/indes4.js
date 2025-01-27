9a0978526f67312d8580b60a207aa117cdfa50a8
Palki Khare <palkikhare2@gmail.com>
2025-01-27 19:44:47 +0530

    FSD

--- javascript/indes4.js
+++ javascript/indes4.js
@@ -1,5 +1,10 @@
-let a= document.querySelector('a')
-a.setAttribute('href','http://youtube.com')
+// let a= document.querySelector('a')
+// a.setAttribute('href','http://youtube.com')
 
 
-console.log(a.getAttribute('href'));
+// console.log(a.getAttribute('href'));
+
+let h1 = document.createElement('h1')
+let div =document.querySelector('div')
+h1.innerText='hello'
+div.appendChild(h1)
