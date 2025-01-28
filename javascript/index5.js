9baf14edc78187c1faccb5d3af91bde18b917189
Palki Khare <palkikhare2@gmail.com>
2025-01-28 19:39:59 +0530

    FSD

---
+++ javascript/index5.js
@@ -0,0 +1,55 @@
+// function fun1(){
+//     console.log("helloooo");
+    
+// }
+
+// let btn= document.querySelector('#one')
+
+// btn.onclick=function(){
+//     console.log('byeeee');
+    
+// }
+
+// let btn1= document.querySelector('#two')
+// let body= document.querySelector('body')
+// btn.addEventListener('click',()=>{
+    // console.log('jaooooo');
+    // body.style.backgroundColor='red'
+    // alert('HEYY')
+    // h1.innerText='palki'
+    
+    
+// })
+
+// let inp= document.querySelector('input')
+// let h2= document.querySelector('h2')
+
+// inp.addEventListener('input',(e)=>{
+//     console.log(e.target.value,"kyayya");
+//     h2.innerText=e.target.value
+    
+// })
+
+// btn.addEventListener('btn',(e)=>{
+//     console.log(e.target.value,"yyyayya");
+//     h2.innerText=e.target.value
+  
+    
+// })
+let btn = document.querySelector('button')
+let body = document.querySelector('body')
+let count=0 
+btn.addEventListener('click',()=>{
+     console.log('jaooooo');
+     if(count%2==0){
+        body.style.backgroundColor='red';
+        count++
+     }
+     
+    else{
+        body.style.backgroundColor='blue';
+        count++
+    }
+    
+    
+ })
